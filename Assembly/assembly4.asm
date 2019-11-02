ecfa159c19d5664f4716a75a8e9ce4c411ac5dec
Vincent.J <56447459+VincentJ-Plus@users.noreply.github.com>
2019-11-02 16:07:33 +0800

    Update assembly4.asm

--- Assembly/assembly4.asm
+++ Assembly/assembly4.asm
@@ -23,20 +23,20 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-		.data				; define the n
-n:		.word 6				; input the n you want
-		.text
-		mov.w n, R4			; set R4 as a copy of n
- 		mov.w R4, R5		; initialize R5
-MULTI:	dec.w R4			; n-1
-		cmp.w #0, R4		; if n=0
-		jeq OVER			; if so, jump to end
-		mov.w R4, &MPY 		; Load 1st operand R4
-		mov.w R5, &OP2 		; Load 2nd operand R4*R5
-		mov.w RESLO, R5		; assign result to R5
-		jmp MULTI
+			.data				; define the n
+n:			.word 6				; input the n you want
+			.text
+			mov.w n, R4			; set R4 as a copy of n
+ 			mov.w R4, R5		; initialize R5
+MULTI:		dec.w R4			; n-1
+			cmp.w #0, R4		; if n=0
+			jeq OVER			; if so, jump to end
+			mov.w R4, &MPY 		; Load 1st operand R4
+			mov.w R5, &OP2 		; Load 2nd operand R4*R5
+			mov.w RESLO, R5		; assign result to R5
+			jmp MULTI
 
-OVER:	jmp $
+OVER:		jmp $
                                             
 
 ;-------------------------------------------------------------------------------
